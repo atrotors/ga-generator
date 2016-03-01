4b92f3cf7ce6c25e114c591f74c38a75b531f562
atrotors <dev@atrotors.com>
2016-03-01 20:04:31 +1100

    make getGASnippet actually work The functionality is done but there is no UI

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,22 +1,60 @@
 'use strict';
 window.addEventListener('load', function() {
   var gacode = document.querySelector('#gacode');
-  gacode.textContent = getGASnippet('ignored');
+  gacode.textContent = getGASnippet('');
   Prism.highlightElement(gacode);
 
   function getGASnippet(param_list) {
-    // ignore param_list for now
+    // html template with placeholders
     var template = '<script>\n  (function(_params){_p0[\'GoogleAnalyticsObject\']=_p4;_p0[_p4]=_p0[_p4]||function(){\n  (_p0[_p4].q=_p0[_p4].q||[]).push(arguments)},_p0[_p4].l=1*new Date();_p5=_p1.createElement(_p2),\n  _p6=_p1.getElementsByTagName(_p2)[0];_p5.async=1;_p5.src=_p3;_p6.parentNode.insertBefore(_p5,_p6)\n  })(_args);\n\n  ga(\'create\', \'UA-XXXXXXXX-X\', \'auto\');\n  ga(\'send\', \'pageview\');\n\n</script>';
     var args = ['window', 'document', '\'script\'', '\'//www.google-analytics.com/analytics.js\'', '\'ga\'']
-    template = template.replace(/_p0/g, 'e');
-    template = template.replace(/_p1/g, 'm');
-    template = template.replace(/_p2/g, 'p');
-    template = template.replace(/_p3/g, 't');
-    template = template.replace(/_p4/g, 'y');
-    template = template.replace(/_p5/g, 'p');
-    template = template.replace(/_p6/g, 'g');
-    template = template.replace(/_params/g, 'e,m,p,t,y,p,g');
-    template = template.replace(/_args/g, args.join(', '));
+
+    // replace the placeholders with variable names or
+    // if not enough params in param_list, just hardcode
+    // the value
+    if (param_list.length == 0) {
+      template = template.replace(/_p0/g, args[0]);
+    } else {
+      template = template.replace(/_p0/g, param_list[0]);
+    }
+    if (param_list.length <= 1) {
+      template = template.replace(/_p1/g, args[1]);
+    } else {
+      template = template.replace(/_p1/g, param_list[1]);
+    }
+    if (param_list.length <= 2) {
+      template = template.replace(/_p2/g, args[2]);
+    } else {
+      template = template.replace(/_p2/g, param_list[2]);
+    }
+    if (param_list.length <= 3) {
+      template = template.replace(/_p3/g, args[3]);
+    } else {
+      template = template.replace(/_p3/g, param_list[3]);
+    }
+    if (param_list.length <= 4) {
+      template = template.replace(/_p4/g, args[4]);
+    } else {
+      template = template.replace(/_p4/g, param_list[4]);
+    }
+    if (param_list.length <= 5) {
+      template = template.replace(/_p5/g, 'a');
+    } else {
+      template = template.replace(/_p5/g, param_list[5]);
+    }
+    if (param_list.length <= 6) {
+      template = template.replace(/_p6/g, 'b');
+    } else {
+      template = template.replace(/_p6/g, param_list[6]);
+    }
+
+    // if param_list.length less that 5, remove
+    // the extra args that will have hardcoded value
+    if (param_list.length < 5) {
+      args = args.slice(0, args.length - (5 - param_list.length))
+    }
+    template = template.replace(/_args/g, args.join(','));
+    template = template.replace(/_params/g, param_list.split('').join(','));
 
     return template;
   }
